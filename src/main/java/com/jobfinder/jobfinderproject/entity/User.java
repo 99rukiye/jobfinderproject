c7a57a4530a46ff3199e5819c60a8d4cfaaedac2
Rukiye TAPAN <tapanmelek99@gmail.com>
2025-08-01 05:41:11 +0300

    User

--- src/main/java/com/jobfinder/jobfinderproject/entity/User.java
+++ src/main/java/com/jobfinder/jobfinderproject/entity/User.java
@@ -2,6 +2,9 @@ package com.jobfinder.jobfinderproject.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.util.Set;
+
 @Table(name = "[user]")
 @Entity
 @Data
@@ -15,24 +18,30 @@ public class User {
     private Long id;
 
     private String fullName;
-
     private String phone;
-
     private String experience;
-
     private String militaryStatus;
-
     private String cvText;
-
     private String email;
-
     private String password;
-
     private boolean blocked = false;
-
     private int failedLoginAttempts = 0;
 
+    // ✅ Roller (ManyToMany ilişkisi)
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles;
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+    // ✅ Admin ile ilişki (eğer her user bir admine bağlıysa)
     @ManyToOne
-    @JoinColumn(name = "admin_id")
+    @JoinColumn(name = "admin_id", referencedColumnName = "id", nullable = true)
     private Admin admin;
 }
