d062665533887a86ae36792440bf74b6033fb2ec
Rukiye TAPAN <tapanmelek99@gmail.com>
2025-08-02 02:28:07 +0300

    Role

---
+++ src/main/java/com/jobfinder/jobfinderproject/entity/Role.java
@@ -0,0 +1,23 @@
+package com.jobfinder.jobfinderproject.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Set;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users;
+}
